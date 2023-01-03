9ef4975b152f90d550c1934da488da647bc59ad0
Mizanur Rahman <mizanur.rahman@bizzntek.com>
2023-01-03 10:31:31 +0600

    Role Based Authentication Project Initial

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoleBaseIdentiyProject.Startup))]
+namespace RoleBaseIdentiyProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
